358c949b729f173d7b726885fe7a3285a544a61e
naijun0403 <65700898+archethic@users.noreply.github.com>
2022-02-23 23:39:13 +0900

    first commit

---
+++ nodejs/process.js
@@ -0,0 +1,25 @@
+/**
+ * Created by naijun on 2022/02/23
+ * Copyright (c) naijun.
+ * This code is licensed under the MIT Licensing Principles.
+ */
+
+ const nodeVersion = 'v9.0.0'
+
+ module.exports = {
+     version: nodeVersion, // not real version sorry :(
+     versions: {
+         node: nodeVersion,
+         java: java.lang.System.getProperty("java.specification.version")
+     },
+     arch: java.lang.System.get("os.arch"),
+     platform: 'linux', // 안드로이드로 하면 뭔가 호환성 에러생길듯
+     release: {
+         name: 'node',
+         sourceUrl: 'https://arthic.dev',
+         headersUrl: 'https://arthic.dev',
+         libUrl: 'https://arthic.dev'
+     },
+     env: java.lang.System.getenv(),
+     title: 'SkyLine Project'
+ }
