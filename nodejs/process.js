eeecb76bf95640f7cf4c94bc221fbb4f387e3ece
naijun0403 <65700898+archethic@users.noreply.github.com>
2022-02-24 00:29:06 +0900

    Update process.js

--- nodejs/process.js
+++ nodejs/process.js
@@ -4,22 +4,61 @@
  * This code is licensed under the MIT Licensing Principles.
  */
 
- const nodeVersion = 'v9.0.0'
+const nodeVersion = 'v9.0.0'
 
- module.exports = {
-     version: nodeVersion, // not real version sorry :(
-     versions: {
-         node: nodeVersion,
-         java: java.lang.System.getProperty("java.specification.version")
-     },
-     arch: java.lang.System.getProperty("os.arch"),
-     platform: 'linux', // 안드로이드로 하면 뭔가 호환성 에러생길듯
-     release: {
-         name: 'node',
-         sourceUrl: 'https://arthic.dev',
-         headersUrl: 'https://arthic.dev',
-         libUrl: 'https://arthic.dev'
-     },
-     env: java.lang.System.getenv(),
-     title: 'SkyLine Project'
- }
+Object.defineProperty(exports, 'version', {
+    enumerable: true,
+    get: function () {
+        return nodeVersion;
+    }
+});
+
+Object.defineProperty(exports, 'versions', {
+    enumerable: true,
+    get: function () {
+        return {
+            node: nodeVersion,
+            java: java.lang.System.getProperty("java.specification.version")
+        }
+    }
+});
+
+Object.defineProperty(exports, 'arch', {
+    enumerable: true,
+    get: function () {
+        return java.lang.System.getProperty("os.arch");
+    }
+});
+
+Object.defineProperty(exports, 'platform', {
+    enumerable: true,
+    get: function () {
+        return 'linux';
+    }
+});
+
+Object.defineProperty(exports, 'release', {
+    enumerable: true,
+    get: function () {
+        return {
+            name: 'node',
+            sourceUrl: 'https://arthic.dev',
+            headersUrl: 'https://arthic.dev',
+            libUrl: 'https://arthic.dev'
+        }
+    }
+});
+
+Object.defineProperty(exports, 'env', {
+    enumerable: true,
+    get: function () {
+        return java.lang.System.getenv();
+    }
+});
+
+Object.defineProperty(exports, 'title', {
+    enumerable: 'title',
+    get: function () {
+        return 'SkyLine Project';
+    }
+});
