a50e065f55657ee33617bb7d6dc15dc8a1e9a581
naijun0403 <naijun0403@naver.com>
2022-03-03 23:26:04 +0900

    fixed

--- nodejs/stream.js
+++ nodejs/stream.js
@@ -25,11 +25,11 @@ var EE = require('events').EventEmitter;
 var util = require('util');
 
 util.inherits(Stream, EE);
-Stream.Readable = require('./_stream_readable');
-Stream.Writable = require('./_stream_writable');
-Stream.Duplex = require('./_stream_duplex');
-Stream.Transform = require('./_stream_transform');
-Stream.PassThrough = require('./_stream_passthrough');
+Stream.Readable = require('./stream_readable');
+Stream.Writable = require('./stream_writable');
+Stream.Duplex = require('./stream_duplex');
+Stream.Transform = require('./stream_transform');
+Stream.PassThrough = require('./stream_passthrough');
 
 // Backwards-compat with node 0.4.x
 Stream.Stream = Stream;
